403a5a1756f8523647c4a2a9fe3642afa091fa64
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2025-12-05 12:32:00 -0600

    work on making 16x16 grid (need pseudocode 1st)

---
+++ script.js
@@ -0,0 +1,6 @@
+// make 16x16 grid of square divs
+for (i = 0; i < 256; i++) {
+    let newGridBox = document.createElement("div");
+    const container = document.getElementById("container");
+    container.appendChild(newGridBox);
+};
