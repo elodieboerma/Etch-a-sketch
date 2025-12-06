e1ebfb01ff62395f64c2e9af501fcec0dc1bdecd
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2025-12-06 13:53:51 -0600

    write pseudocode

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // make 16x16 grid of square divs
-for (i = 0; i < 256; i++) {
+/*for (i = 0; i < 256; i++) {
     let newGridBox = document.createElement("div");
     const container = document.getElementById("container");
     container.appendChild(newGridBox);
-};
+};*/
+
+//select #container
+//let numSquares = 16
+
+//do this 16x; then the whole thing 16x again (or just do it 256x???)
+//make div element
+//add class gridSquare
+//add as child to #container
+
+//eventListener for mouseenter -- if not then try onmouseenter, or mouseover or mousemove
+    //add .changeColor to element
+
+//select button
+//numSquares = prompt("questionHere")
+//run the js to make the grid again with the new numSquares values, but no change in total pixels used
