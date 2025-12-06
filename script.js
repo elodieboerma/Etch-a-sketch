98444b3b3761a2ae91cf8ada535bc8cf9b2f308e
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2025-12-06 14:26:24 -0600

    make basic skeletons using pseudocode in all files
    -also made notes in css about figuring out how to get height/width as desired for little sqaure boxes
    -have not tested anything at all yet

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
-// make 16x16 grid of square divs
-/*for (i = 0; i < 256; i++) {
+/* make 16x16 grid of square divs
+for (i = 0; i < 256; i++) {
     let newGridBox = document.createElement("div");
     const container = document.getElementById("container");
     container.appendChild(newGridBox);
 };*/
 
 //select #container
+const container = document.getElementById("container");
 //let numSquares = 16
+let numSquares = 16;
 
-//do this 16x; then the whole thing 16x again (or just do it 256x???)
-//make div element
-//add class gridSquare
-//add as child to #container
+function makeGrid(numSquaresInARow) {
+    //do this 16x; then the whole thing 16x again (or just do it 256x???)
+    for(i = 0; i < 16; i++) {
+        for(i = 0; i < 16; i++) {
+            //make div element
+            let newBox = document.createElement("div");
+            //add class gridSquare
+            newBox.addClass("gridSquare");
+            //add as child to #container
+            container.appendChild(newBox);
 
-//eventListener for mouseenter -- if not then try onmouseenter, or mouseover or mousemove
-    //add .changeColor to element
+            //eventListener for mouseenter -- if not then try onmouseenter, or mouseover or mousemove
+            newBox.addEventListener("mouseenter", () => newBox.addClass("changeColor"));
+                //add .changeColor to element
+        };
+    };
+};
 
 //select button
-//numSquares = prompt("questionHere")
-//run the js to make the grid again with the new numSquares values, but no change in total pixels used
+const button = document.querySelector("button");
+
+makeGrid(numSquares);
+
+//listen for when button clicked
+button.addEventListener("click", (/*numSquares not sure if needed here*/) => {
+    //numSquares = prompt("questionHere")
+    numSquares = prompt("How many squares per row?")
+    //run the js to make the grid again with the new numSquares values, but no change in total 
+    // pixels used, need to use a function somehow???
+    makeGrid(numSquares);
+});
